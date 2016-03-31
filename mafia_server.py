25f76a26f0c9eebe913535177d8cf9554e7335af
Yash Srivastav <yashsriv01@gmail.com>
2016-03-31 23:31:08 +0530

    Added error code to be returned

--- mafia_server.py
+++ mafia_server.py
@@ -1,7 +1,7 @@
 """
 Server for mafia
 """
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort
 APP = Flask(__name__)
 CLIENTS = []
 GAME_STARTED = False
@@ -19,23 +19,32 @@ def status():
     Status Page
     """
     message = 'Welcome To Mafia Reloaded\n'
-    if GAME_STARTED:
-        message += 'Game Has Been Started\n'
-        message += 'Connected Players :\n'
-        message += ('\n').join(CLIENTS)
+    message = 'MAX_NUM = ' + str(MAX_NUM) + '\n'
+    message += 'CLIENTS = ' + str(CLIENTS) + '\n'
+    message += 'GAME_STARTED = ' + str(GAME_STARTED) + '\n'
     return message
 
 @APP.route('/main/', methods=['GET', 'POST'])
-def add_user():
+@APP.route('/main/<username>', methods=['GET'])
+def add_user(username=None):
     """
     Adds client to current users
+
+    Using either POST request to main
+    or GET request to main/username
     """
     if request.method == 'POST':
-        if request.form['user'] not in CLIENTS and len(CLIENTS) < MAX_NUM:
-            CLIENTS.append(request.form['user'])
-            if len(CLIENTS) == MAX_NUM:
-                global GAME_STARTED
-                GAME_STARTED = True
+        username = request.form['user']
+    if username not in CLIENTS and len(CLIENTS) < MAX_NUM:
+        CLIENTS.append(username)
+    if len(CLIENTS) == MAX_NUM:
+        global GAME_STARTED
+        GAME_STARTED = True
+    if username in CLIENTS and GAME_STARTED:
+        return ('###').join(CLIENTS)
+    else:
+        abort(501)
+
 
 if __name__ == '__main__':
     global MAX_NUM
